1306197632f4fe0263fc03962fadff4c0a643dc1
Daren Yong <daren.yong@sjrb.ca>
2018-03-10 22:35:15 -0600

    log full req

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 // home page
 router.get('/', function (req, res, next) {
-  const { headers, originalUrl, url } = req;
-  log.info(`GET auth ${originalUrl} ${url}`, headers);
+  const { headers } = req;
+  log.info(`GET auth r: ${JSON.stringify(req)}`, headers);
   res.json({title: 'auth hello'});
 });
 
