f651460b649fbaf69be3488e33cd4cc2f580e627
Daren Yong <daren.yong@sjrb.ca>
2018-03-10 22:32:28 -0600

    log url

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 // home page
 router.get('/', function (req, res, next) {
-  const { headers } = req;
-  log.info('GET auth', headers);
+  const { headers, originalUrl, url } = req;
+  log.info(`GET auth ${originalUrl} ${url}`, headers);
   res.json({title: 'auth hello'});
 });
 
