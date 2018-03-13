2f347273e99179b373d6c2fa7dfdbc92ba6eccdd
Daren Yong <daren.yong@sjrb.ca>
2018-03-12 21:00:22 -0600

    fix redirect

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.get('/', function (req, res, next) {
   const { headers } = req;
   log.info('GET auth', headers);
-  const uri = req.get('X-Forwarded-Uri');
+  const proto = req.get('x-forwarded-proto');
+  const host = req.get('x-forwarded-host');
+  const uri = req.get('x-forwarded-uri');
   console.log('cookies', req.cookies);
   const cookie = req.cookies['daren-auth-token'];
   console.log('daren-auth-token', cookie);
@@ -15,7 +17,9 @@ router.get('/', function (req, res, next) {
     const secure = true;
     const maxAge = 60;
     res.cookie('daren-auth-token', 'myJwt', { domain, secure, maxAge });
-    res.redirect(uri);
+    const goto = `${proto}:\\${host}${uri}`;
+    console.log('redirect to', goto);
+    res.redirect(goto);
     return;
   }
   res.json({ title: 'auth hello', uri });
