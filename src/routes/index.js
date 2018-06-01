3a7b8fa98f7581112df69fa78d731d00f4e48199
Daren Yong <daren.yong@sjrb.ca>
2018-05-31 20:43:34 -0600

    attempt get auth0 code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+// const request = require('superagent');
+const querystring = require('querystring');
+const fs = require('fs');
+const path = require('path');
+
+const audience='https://darenyong.com/jenkins';
+const scope='read:jenkins';
+const response_type='code';
+const client_id=fs.readFileSync(path.join(__dirname, '..', '..', 'client_id'), 'utf-8');
+const redirect_uri='https://darenyong.com/auth/callback';
+const state='goofy';
+
+const authUrl = 'https://darenyong.auth0.com/authorize?'
+  + querystring.stringify({audience, scope, response_type, client_id, redirect_uri, state});
+
+router.get('/callback', function (req, res, next) {
+  res.json({msg: 'auth callback'});
+});
+
 
 // home page
 router.get('/', function (req, res, next) {
@@ -17,16 +36,20 @@ router.get('/', function (req, res, next) {
   const cookie = req.cookies['daren-auth-token'];
   console.log('daren-auth-token', cookie);
 
+
   try {
-    if (uri === '/jenkins' && cookie !== 'myJwt') {
-      const goto = `${proto}://${host}${uri}`;
+    // if (uri === '/jenkins' && cookie !== 'myJwt') {
+    if (uri === '/jenkins') {
+      // const goto = `${proto}://${host}${uri}`;
+      const goto = authUrl;
+
       console.log('redirect to', goto);
 
-      const domain = 'darenyong.com';
-      const secure = false;
-      const maxAge = 60000;
-      const httpOnly = false;
-      res.cookie('daren-auth-token', 'myJwt', { secure, maxAge, httpOnly });
+      // const domain = 'darenyong.com';
+      // const secure = false;
+      // const maxAge = 60000;
+      // const httpOnly = false;
+      // res.cookie('daren-auth-token', 'myJwt', { secure, maxAge, httpOnly });
       res.redirect(goto);
       return;
     }
