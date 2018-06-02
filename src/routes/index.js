9cc46c1306f7bec840a66accec07b71d769cb579
Daren Yong <daren.yong@sjrb.ca>
2018-06-01 23:42:32 -0600

    clean up

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,18 +8,18 @@ const path = require('path');
 const cookieName = 'daren-auth-token';
 
 // needed to request code
-const audience='https://darenyong.com/jenkins';
-const scope='read:jenkins';
-const response_type='code';
-const client_id=fs.readFileSync(path.join(__dirname, '..', '..', 'client_id'), 'utf-8');
-const redirect_uri='https://darenyong.com/auth/callback';
-const state='goofy';
+const audience = 'https://darenyong.com/jenkins';
+const scope = 'read:jenkins';
+const response_type = 'code';
+const client_id = fs.readFileSync(path.join(__dirname, '..', '..', 'client_id'), 'utf-8');
+const redirect_uri = 'https://darenyong.com/auth/callback';
+const state = 'goofy';
 
 // needed to request token
-const client_secret=fs.readFileSync(path.join(__dirname, '..', '..', 'client_secret'), 'utf-8');
+const client_secret = fs.readFileSync(path.join(__dirname, '..', '..', 'client_secret'), 'utf-8');
 
 const authUrl = 'https://darenyong.auth0.com/authorize?'
-  + querystring.stringify({audience, scope, response_type, client_id, redirect_uri, state});
+  + querystring.stringify({ audience, scope, response_type, client_id, redirect_uri, state });
 
 const setCookie = (res, token) => {
   console.log('setting new cookie');
@@ -30,35 +30,42 @@ const setCookie = (res, token) => {
 };
 
 router.get('/callback', function (req, res, next) {
-  const queryPart = req.originalUrl.substring( req.originalUrl.indexOf('?') + 1 );
-  const parsed = querystring.parse(queryPart);
-  console.log('callback query parsed', parsed);
+  try {
+    log.info('got auth code, exchange for token');
 
-  // TODO: check parsed.state as nounce to avoid replay attack
+    const queryPart = req.originalUrl.substring(req.originalUrl.indexOf('?') + 1);
+    const parsed = querystring.parse(queryPart);
+    console.log('callback query parsed', parsed);
 
-  const body = {
-    grant_type: 'authorization_code',
-    client_id,
-    client_secret,
-    code: parsed.code,
-    redirect_uri
-  };
+    // TODO: check parsed.state as nounce to avoid replay attack
 
-  console.log('exchanging code for token');
-  request
-    .post('https://darenyong.auth0.com/oauth/token')
-    .set('content-type', 'application/json')
-    .send(body)
-    .then(oauth => {
-      setCookie(res, oauth.body.access_token);
-      console.log('redirecting back to original requested url');
-      res.redirect('https://darenyong.com')
-    })
-    .catch(err => {
-      console.log('error getting token', err);
-      res.status(400);
-      res.send('error getting token');
-    });
+    const body = {
+      grant_type: 'authorization_code',
+      client_id,
+      client_secret,
+      code: parsed.code,
+      redirect_uri
+    };
+
+    request
+      .post('https://darenyong.auth0.com/oauth/token')
+      .set('content-type', 'application/json')
+      .send(body)
+      .then(oauth => {
+        setCookie(res, oauth.body.access_token);
+        console.log('redirecting back to original requested url');
+        res.redirect('https://darenyong.com')
+      })
+      .catch(err => {
+        log.error('error in /oath/token request', err);
+        res.status(500);
+        res.send('error in /oath/token request');
+      });
+  } catch (err) {
+    log.error('error exchanging code for token', err);
+    res.status(500);
+    res.send('error exchanging code for token');
+  }
 });
 
 // home page - all auth requests land here
